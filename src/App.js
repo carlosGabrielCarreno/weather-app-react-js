1bbae730d14fecd0874828f21c991d8a0dcfb221
gabrielCarrenio <carrenocarlosgabriel@gmail.com>
2022-10-30 19:10:49 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Cards from './components/Cards';
 import { ErrorMessage } from './components/ErrorMessage';
 import Nav from './components/Nav';
 import { useFetch } from './hooks/useFetch';
+import './app.css';
 
 const App = () => {
   const { cities, errorMessage, onClose, onErrorMessage, setPlaceName } =
     useFetch();
 
   return (
-    <>
+    <div className="app">
       <Nav onSearch={setPlaceName} />
       {errorMessage.length === 0 ? (
         <Cards cities={cities} onClose={onClose} />
@@ -20,7 +21,7 @@ const App = () => {
           />
         </>
       )}
-    </>
+    </div>
   );
 };
 
